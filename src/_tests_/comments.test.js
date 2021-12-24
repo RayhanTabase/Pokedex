fa2ab4822252dc518fa0146ccd411d9516d0ec7d
Rayhan <nofussdufus@gmail.com>
2021-12-24 05:33:05 +0000

    Add test for get

--- src/_tests_/comments.test.js
+++ src/_tests_/comments.test.js
@@ -1,10 +1,15 @@
 import { countComments } from '../scripts/CommentApi.js';
 
+const array =[
+    {date_created:'21-12-2021',username:'Mike',comment:'Hi there'},
+    {date_created:'21-12-2021',username:'Mike',comment:'Hi there'},
+    {date_created:'21-12-2021',username:'Mike',comment:'Hi there'}
+]
 
 global.fetch = jest.fn(() => Promise.resolve({
     status:200,
     json(){
-      return [{1:"1"},{2:"2"},{3:"3"}];
+      return array;
     }
 }));
 
